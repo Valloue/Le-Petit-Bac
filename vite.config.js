8b35038b6ea57f46f4709fe0761a45aa25e42b14
Valloue <conreauvalentin@gmail.com>
2025-01-03 20:08:59 +0100

    Correction de la configuration Vite

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,28 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import glob from 'fast-glob'
 
 export default defineConfig({
   base: '/Le-Petit-Bac/',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        ...Object.fromEntries(
-          glob.sync('html/**/*.html').map(file => [
-            file.replace(/(\.html)$/, ''),
-            resolve(__dirname, file)
-          ])
-        )
-      },
-    },
+      input: resolve(__dirname, 'index.html'),
+      external: [
+        '/css/style.css',
+        '/js/main.js'
+      ]
+    }
+  },
+  server: {
+    open: true
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './'),
-      '@config': resolve(__dirname, 'config'),
-      '@css': resolve(__dirname, 'css'),
-      '@js': resolve(__dirname, 'js'),
-      '@html': resolve(__dirname, 'html'),
-      '@public': resolve(__dirname, 'public')
-    },
-  },
-  publicDir: 'public'
-}) 
+      '/js': resolve(__dirname, 'js'),
+      '/css': resolve(__dirname, 'css'),
+      '/html': resolve(__dirname, 'html'),
+      '/config': resolve(__dirname, 'config'),
+      '/public': resolve(__dirname, 'public')
+    }
+  }
+})
