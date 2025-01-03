11c21ce7344fb29ab2ba5f124bf692f80deee246
Valloue <conreauvalentin@gmail.com>
2025-01-03 19:49:33 +0100

    Adaptation de la configuration Vite à la structure du projet

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,20 @@ export default defineConfig({
   root: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        // Ajoutez ici d'autres points d'entrée HTML si nécessaire
+      }
+    }
+  },
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@config': '/config',
+      '@css': '/css',
+      '@js': '/js',
+      '@html': '/html'
+    }
   }
 }) 
